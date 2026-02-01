221eca43d8ba2d42c16f65e7709645d45dc8e67d
Patryk777 <patryk.walkiewicz1988@gmail.com>
2026-02-01 20:05:02 +0100

    fix: remove tests from Vercel build, run locally only

--- jest.setup.js
+++ jest.setup.js
@@ -3,29 +3,6 @@ import '@testing-library/jest-dom';
 // Fix for React 19 - globalThis.IS_REACT_ACT_ENVIRONMENT
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
-// React 19 moved act from react-dom/test-utils to react
-// This polyfill ensures compatibility
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-if (typeof React.act === 'undefined') {
-  // For React 19, act should be available on React
-  // If not, we need to create a proper implementation
-  const { act: reactDomAct } = require('react-dom/test-utils');
-  if (reactDomAct) {
-    React.act = reactDomAct;
-  } else {
-    // Fallback implementation
-    React.act = (callback) => {
-      const result = callback();
-      if (result && typeof result.then === 'function') {
-        return result;
-      }
-      return Promise.resolve(result);
-    };
-  }
-}
-
 // Mock localStorage
 const localStorageMock = {
   getItem: jest.fn(),
